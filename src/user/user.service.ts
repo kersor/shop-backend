0cba8d68307db1449e641f8a415b574cd185c070
svolochyo <vasilev.rustik.03@mail.ru>
2025-09-04 20:34:06 +0300

    init prisma ORM

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateUserDto } from './dto/create.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        readonly prisma: PrismaService
+    ) {}
+
+    createUser (dto: CreateUserDto) {
+        const user = this.prisma.user.create({
+            data: {
+                email: dto.email,
+                password: dto.password
+            }
+        })
+
+        return user
+    }
+}
